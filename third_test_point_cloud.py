fc2769dccfd71da9e14f277fe34a951374f5e223
michaelBezick <mbezick@purdue.edu>
2025-01-23 17:37:10 -0500

    Auto commit

--- third_test_point_cloud.py
+++ third_test_point_cloud.py
@@ -13,11 +13,16 @@ depth_map = physics.render(height=480, width=640, depth=True, camera_id=0)
 rgb_image = physics.render(height=480, width=640, camera_id=0)
 
 
-np.set_printoptions(threshold=sys.maxsize)
 print(np.shape(depth_map))
 print(np.max(depth_map))
 print(np.min(depth_map))
 print(np.mean(depth_map))
+
+min_depth, max_depth = 0, 10
+clipped_depth = np.clip(depth_map, min_depth, max_depth)
+normalized_depth = (clipped_depth - min_depth) / (max_depth - min_depth)
+depth_map = normalized_depth
+
 
 data = depth_map.flatten()
 counts, bins = np.histogram(data, bins=100, range=(0, 100))
