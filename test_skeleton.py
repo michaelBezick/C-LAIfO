9857f59f19176cfd5aa44aa79beb20defeeab0bf
michaelBezick <mbezick@purdue.edu>
2025-03-03 12:13:14 -0500

    Auto commit

--- test_skeleton.py
+++ test_skeleton.py
@@ -37,7 +37,7 @@ from point_cloud_generator import PointCloudGenerator
 
 pcg = PointCloudGenerator(sim=physics)
 
-point_cloud = pcg.depthImageToPointCloud(depth, 0)
+point_cloud = pcg.depthImageToPointCloud(depth, 0, down_sample_voxel_size=0.002)
 
 new_point_cloud = o3d.geometry.PointCloud()
 new_point_cloud.points = o3d.utility.Vector3dVector(point_cloud)
@@ -63,8 +63,8 @@ lbc.extract_topology()
 time2 = time.time()
 
 print(time2 - time1)
-lbc.visualize()
+# lbc.visualize()
 lbc.export_results('./output')
-lbc.animate(init_rot=np.asarray([[1, 0, 0], [0, 0, 1], [0, 1, 0]]),
-            steps=300,
-            output='./output')
+# lbc.animate(init_rot=np.asarray([[1, 0, 0], [0, 0, 1], [0, 1, 0]]),
+#             steps=300,
+#             output='./output')
--- test_skeleton_from_file.py
+++ test_skeleton_from_file.py
@@ -5,17 +5,19 @@ import numpy as np
 from pc_skeletor import LBC
 import time
 
-point_cloud = o3d.io.read_point_cloud("./point_cloud_images/filtered_point_cloud.ply")
+point_cloud = o3d.io.read_point_cloud("./point_cloud_images/random_point_cloud_filtered.ply")
+print(np.asarray(point_cloud.points).shape)
+exit()
 
 time1 = time.time()
-lbc = LBC(point_cloud=point_cloud, down_sample=0.008)
+lbc = LBC(point_cloud=point_cloud, down_sample=0.001)
 lbc.extract_skeleton()
 lbc.extract_topology()
 time2 = time.time()
 
 print(time2 - time1)
-lbc.visualize()
+# lbc.visualize()
 lbc.export_results('./output')
-lbc.animate(init_rot=np.asarray([[1, 0, 0], [0, 0, 1], [0, 1, 0]]),
-            steps=300,
-            output='./output')
+# lbc.animate(init_rot=np.asarray([[1, 0, 0], [0, 0, 1], [0, 1, 0]]),
+#             steps=300,
+#             output='./output')
