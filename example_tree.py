0ebb5e2c5373460271ed0cd7d35791a9571ead89
michaelBezick <mbezick@purdue.edu>
2025-03-03 10:43:34 -0500

    Squashed 'external/pc-skeletor/' content from commit 5ecc210
    git-subtree-dir: external/pc-skeletor
    git-subtree-split: 5ecc2105edbe28a6ca33e7674dfd984d6f3e10d4

---
+++ example_tree.py
@@ -0,0 +1,44 @@
+import open3d as o3d
+import numpy as np
+
+from pc_skeletor import SLBC, LBC
+from pc_skeletor import Dataset
+
+if __name__ == "__main__":
+    downloader = Dataset()
+    trunk_pcd_path, branch_pcd_path = downloader.download_semantic_tree_dataset()
+
+    pcd_trunk = o3d.io.read_point_cloud(trunk_pcd_path)
+    pcd_branch = o3d.io.read_point_cloud(branch_pcd_path)
+    pcd = pcd_trunk + pcd_branch
+
+    # Laplacian-based Contraction
+    lbc = LBC(point_cloud=pcd,
+              init_contraction=2,
+              init_attraction=0.5,
+              down_sample=0.03)
+    lbc.extract_skeleton()
+    lbc.extract_topology()
+    lbc.visualize()
+    lbc.show_graph(lbc.skeleton_graph, fig_size=(30, 30))
+    lbc.show_graph(lbc.topology_graph)
+    lbc.export_results('./output')
+    lbc.animate(init_rot=np.asarray([[1, 0, 0], [0, 0, 1], [0, 1, 0]]), steps=500, output='./output_lbc')
+    #lbc.animate_contracted_pcd(init_rot=np.asarray([[1, 0, 0], [0, 0, 1], [0, 1, 0]]), steps=300, output='./output')
+    lbc.animate_topology(init_rot=np.asarray([[1, 0, 0], [0, 0, 1], [0, 1, 0]]), steps=300, output='./output')
+
+    # Semantic Laplacian-based Contraction
+    s_lbc = SLBC(point_cloud={'trunk': pcd_trunk, 'branches': pcd_branch},
+                 semantic_weighting=30.,
+                 init_contraction=1,
+                 init_attraction=0.5,
+                 down_sample=0.009)
+    s_lbc.extract_skeleton()
+    s_lbc.extract_topology()
+    s_lbc.visualize()
+    s_lbc.show_graph(s_lbc.skeleton_graph, fig_size=(30, 30))
+    s_lbc.show_graph(s_lbc.topology_graph)
+    s_lbc.export_results('./output_slbc')
+    s_lbc.animate(init_rot=np.asarray([[1, 0, 0], [0, 0, 1], [0, 1, 0]]), steps=500, output='./output')
+    #s_lbc.animate_contracted_pcd(init_rot=np.asarray([[1, 0, 0], [0, 0, 1], [0, 1, 0]]), steps=300, output='./output')
+    s_lbc.animate_topology(init_rot=np.asarray([[1, 0, 0], [0, 0, 1], [0, 1, 0]]), steps=300, output='./output_slbc')
---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2022 Lukas Meyer
+Licensed under the MIT License.
+See LICENSE file for more information.
+"""
+
+# Built-in/Generic Imports
+import setuptools
+from setuptools import find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'Readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
+
+setuptools.setup(
+    name='pc_skeletor',
+    version='1.0.1',
+    description='Point Cloud Skeletonizer',
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Lukas Meyer',
+    author_email='lukas.meyer@fau.de',
+    url="https://github.com/meyerls/PC-Skeletor",
+    packages=find_packages(),
+    install_requires=install_requires,  # external packages as dependencies
+    python_requires='>=3.8',
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    zip_safe=False,
+)
