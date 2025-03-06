2df414471f12b6e4fbe38bffad5b56b0685861f9
michaelBezick <mbezick@purdue.edu>
2025-03-06 15:39:05 -0500

    Auto commit

--- final_experiments/plot_results.py
+++ final_experiments/plot_results.py
@@ -1,8 +1,8 @@
 
 import matplotlib.pyplot as plt
 
-eval_path = "./medium_point_net,less_mismatch,rotation_aug/1934_CL_data_type=agent,agent=lail_cl_multiset,apply_aug=CL-Q,aug_type=brightness,batch_size=256,delta_source=0.2,delta_target=-0.25,depth_flag=True,device=cuda:1,difficulty=easy,lr=1e-4,task_agent=walker_walk,task_expert=walker_walk/eval.csv"
-train_path = "./medium_point_net,less_mismatch,rotation_aug/1934_CL_data_type=agent,agent=lail_cl_multiset,apply_aug=CL-Q,aug_type=brightness,batch_size=256,delta_source=0.2,delta_target=-0.25,depth_flag=True,device=cuda:1,difficulty=easy,lr=1e-4,task_agent=walker_walk,task_expert=walker_walk/train.csv"
+eval_path = "./medium_point_net,less_mismatch,no_rotation_aug/1143_CL_data_type=agent,agent=lail_cl_multiset,apply_aug=CL-Q,aug_type=brightness,batch_size=256,delta_source=0.2,delta_target=-0.25,depth_flag=True,device=cuda:2,difficulty=easy,lr=1e-4,task_agent=walker_walk,task_expert=walker_walk/eval.csv"
+train_path = "./medium_point_net,less_mismatch,no_rotation_aug/1143_CL_data_type=agent,agent=lail_cl_multiset,apply_aug=CL-Q,aug_type=brightness,batch_size=256,delta_source=0.2,delta_target=-0.25,depth_flag=True,device=cuda:2,difficulty=easy,lr=1e-4,task_agent=walker_walk,task_expert=walker_walk/train.csv"
 
 eval_reward = []
 eval_step = []
@@ -32,10 +32,11 @@ with open(train_path, "r") as file:
         train_step.append(float(step))
 
 plt.figure()
-plt.title("Point Cloud Small Mismatch Train vs. Eval on Walker Walk")
+plt.title("Point Cloud Small Mismatch Train vs. Eval on Walker Walk - No Aug")
 plt.xlabel("Steps")
 plt.ylabel("Reward")
 plt.plot(train_step, train_reward, label="Train")
 plt.plot(eval_step, eval_reward, label="Eval")
 plt.legend()
-plt.savefig("./plots/point_cloud_large_no_mismatch.pdf")
+plt.savefig("./plots/point_cloud_small_mismatch_no_aug.pdf")
+plt.savefig("./plots/point_cloud_small_mismatch_no_aug.jpg",dpi=600)
